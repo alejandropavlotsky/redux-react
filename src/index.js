1fcb8d5279a29544e488961e960d4a03e2b505a8
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-01-07 13:58:40 +0100

    ADD reducer, ListItem, todos, store

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import ListItem from './components/ListItem';
+import Input from './components/Input';
+import { complete, submit } from './reducers/todos';
+
+const App = ({ data, complete, submit }) => {
+
+  const [ value, setValue ] = useState('')
+
+  const handleChange = (val) => {
+    setValue(val)
+  }
+
+  const handleSubmit = () => {
+    submit(value) 
+    setValue('')
+  }
+
+  return (
+     <View style={styles.container}>
+        <Input 
+          onSubmit={handleSubmit}
+          onChange={handleChange}
+          value={value}
+        />
+
+        <FlatList 
+          style={styles.list}
+          data={data}
+          keyExtractor={x => String(x.id)}
+          renderItem={({ item }) => 
+            <ListItem completed={item.completed} onPress={() => complete(item.id)} desc={item.desc}/>}
+        />
+
+    </View>
+  )
+}
+
+const mapStateToProps = state => {
+  return {data: state.todos}
+}
+
+const mapDispatchToProps = dispatch => ({
+  complete: (id) => dispatch(complete(id)),
+  submit: (val) => dispatch(submit(val)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 35,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  list: {
+    alignSelf: 'stretch',
+  },
+});
